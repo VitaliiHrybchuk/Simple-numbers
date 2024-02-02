dbafd6a8127f90c95b799b892921bdea172dc029
Grib <grib.ua@ukr.net>
2024-02-02 20:06:09 +0000

    Is done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 'use strict';
 
-
-for(let i = 1; i <= 100; i++){
-  (i % 1 === 0) && (i % i === 0) &&
-  // i === 101; break;
-  console.log(`${i}`);
-
-
+first:for (let i = 2; i <= 100; i++){
+  for (let j = 2; j < i; j++){
+    if (i % j === 0){
+      continue first;
+    }
+  }
+console.log(`${i} is a simple number`);
 }
-  
